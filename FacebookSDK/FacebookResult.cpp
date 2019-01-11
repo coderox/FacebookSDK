1ea4551b86a7bb72e8f0dde49082609e764428ab
Johan Lindfors <johan.lindfors@coderox.se>
2019-01-11 15:00:03 +0100

    FacebookResult implemented

---
+++ FacebookSDK/FacebookResult.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "FacebookResult.h"
+
+namespace winrt::FacebookSDK::implementation
+{
+    FacebookResult::FacebookResult(Windows::Foundation::IInspectable const& Object)
+		: _error(nullptr)
+		, _object(nullptr)
+    {
+		_error = Object.as<winrt::FacebookSDK::FacebookError>();
+		if (!_error) {
+			// Not an error, save as our object
+			_object = Object;
+		}
+	}
+
+	FacebookResult::~FacebookResult() {
+
+	}
+
+    bool FacebookResult::Succeeded()
+    {
+		return _object != nullptr;
+    }
+
+    Windows::Foundation::IInspectable FacebookResult::Object()
+    {
+		return _object;
+    }
+
+    FacebookSDK::FacebookError FacebookResult::ErrorInfo()
+    {
+		return _error;
+	}
+}
---
+++ FacebookSDK/FacebookResult.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "FacebookError.h"
+#include "FacebookResult.g.h"
+
+namespace winrt::FacebookSDK::implementation
+{
+    struct FacebookResult : FacebookResultT<FacebookResult>
+    {
+	public:
+        FacebookResult() = delete;
+        FacebookResult(Windows::Foundation::IInspectable const& Object);
+
+        bool Succeeded();
+        Windows::Foundation::IInspectable Object();
+        FacebookSDK::FacebookError ErrorInfo();
+	private:
+		~FacebookResult();
+
+		Windows::Foundation::IInspectable _object;
+		FacebookSDK::FacebookError _error;
+    };
+}
+
+namespace winrt::FacebookSDK::factory_implementation
+{
+    struct FacebookResult : FacebookResultT<FacebookResult, implementation::FacebookResult>
+    {
+    };
+}
