712aa6ff828f4591e6f42c1e7435bbecf0abafd3
Johan Lindfors <johan.lindfors@coderox.se>
2019-01-13 22:24:37 +0100

    More work on IHttpClient

--- FacebookSDK.Tests/HttpClientFake.cpp
+++ FacebookSDK.Tests/HttpClientFake.cpp
@@ -1,32 +1,31 @@
 #include "pch.h"
-#include "../FacebookSDK/IHttpClient.h"
 
 namespace FacebookSDK_Tests
 {
 	public ref class HttpClientFake sealed : FacebookSDK::IHttpClient {
 	public:
-		Windows::Foundation::IAsyncOperation<Platform::String^>^ GetTaskAsync(
+		virtual Windows::Foundation::IAsyncOperation<Platform::String^>^ GetTaskAsync(
 			Platform::String^ path,
 			Windows::Foundation::Collections::IMapView<Platform::String^, Platform::Object^>^ parameters
 		) {
 			throw ref new Platform::NotImplementedException();
 		}
 
-		Windows::Foundation::IAsyncOperation<Platform::String^>^ PostTaskAsync(
+		virtual Windows::Foundation::IAsyncOperation<Platform::String^>^ PostTaskAsync(
 			Platform::String^ path,
 			Windows::Foundation::Collections::IMapView<Platform::String^, Platform::Object^>^ parameters
 		) {
 			throw ref new Platform::NotImplementedException();
 		}
 
-		Windows::Foundation::IAsyncOperation<Platform::String^>^ DeleteTaskAsync(
+		virtual Windows::Foundation::IAsyncOperation<Platform::String^>^ DeleteTaskAsync(
 			Platform::String^ path,
 			Windows::Foundation::Collections::IMapView<Platform::String^, Platform::Object^>^ parameters
 		) {
 			throw ref new Platform::NotImplementedException();
 		}
 
-		Platform::String^ ParametersToQueryString(
+		virtual Platform::String^ ParametersToQueryString(
 			Windows::Foundation::Collections::IMapView<Platform::String^, Platform::Object^>^ parameters
 		) {
 			throw ref new Platform::NotImplementedException();
