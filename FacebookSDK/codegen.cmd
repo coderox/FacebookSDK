60daeae985c108be6fcfcb58d2edb4f81decd75c
Johan Lindfors <johan.lindfors@coderox.se>
2019-01-15 21:40:22 +0100

    FBPermission

--- FacebookSDK/codegen.cmd
+++ FacebookSDK/codegen.cmd
@@ -25,7 +25,7 @@ call :GenFile Graph.FBGroup.cpp Graph.FBGroup.cpp.tt
 call :GenFile Graph.FBPage.cpp Graph.FBPage.cpp.tt
 REM call :GenFile FBPageCategory.cpp FBPageCategory.cpp.tt
 REM call :GenFile FBPaging.cpp FBPaging.cpp.tt
-REM call :GenFile FBPermission.cpp FBPermission.cpp.tt
+call :GenFile Graph.FBPermission.cpp Graph.FBPermission.cpp.tt
 call :GenFile Graph.FBUser.cpp Graph.FBUser.cpp.tt
 call :GenFile Graph.FBProfilePicture.cpp Graph.FBProfilePicture.cpp.tt
 call :GenFile Graph.FBProfilePictureData.cpp Graph.FBProfilePictureData.cpp.tt
@@ -38,7 +38,7 @@ call :GenFile Graph.FBGroup.h Graph.FBGroup.h.tt
 call :GenFile Graph.FBPage.h Graph.FBPage.h.tt
 REM call :GenFile FBPageCategory.h FBPageCategory.h.tt
 REM call :GenFile FBPaging.h FBPaging.h.tt
-REM call :GenFile FBPermission.h FBPermission.h.tt
+call :GenFile Graph.FBPermission.h Graph.FBPermission.h.tt
 call :GenFile Graph.FBUser.h Graph.FBUser.h.tt
 call :GenFile Graph.FBProfilePicture.h Graph.FBProfilePicture.h.tt
 call :GenFile Graph.FBProfilePictureData.h Graph.FBProfilePictureData.h.tt
@@ -54,6 +54,7 @@ call :GenFile Graph.FBProfilePictureData.idl Graph.FBProfilePictureData.idl.tt
 call :GenFile Graph.FBCursors.idl Graph.FBCursors.idl.tt
 call :GenFile Graph.FBGroup.idl Graph.FBGroup.idl.tt
 call :GenFile Graph.FBObject.idl Graph.FBObject.idl.tt
+call :GenFile Graph.FBPermission.idl Graph.FBPermission.idl.tt
 
 goto End
 
