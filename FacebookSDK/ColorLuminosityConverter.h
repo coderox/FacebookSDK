576a234f6aa326507a91a5b34e51da96fbd5885f
Johan Lindfors <johan.lindfors@coderox.se>
2019-01-29 12:27:18 +0100

    Formatting the converters

--- FacebookSDK/ColorLuminosityConverter.h
+++ FacebookSDK/ColorLuminosityConverter.h
@@ -13,15 +13,15 @@ namespace winrt::FacebookSDK::implementation
 		Windows::UI::Color ApplyFactor(Windows::UI::Color const& color, double factor);
 
 		virtual Windows::Foundation::IInspectable Convert(
-			Windows::Foundation::IInspectable const& value, 
-			Windows::UI::Xaml::Interop::TypeName const& targetType, 
-			Windows::Foundation::IInspectable const& parameter, 
+			Windows::Foundation::IInspectable const& value,
+			Windows::UI::Xaml::Interop::TypeName const& targetType,
+			Windows::Foundation::IInspectable const& parameter,
 			hstring const& language);
-		
+
 		virtual Windows::Foundation::IInspectable ConvertBack(
-			Windows::Foundation::IInspectable const& /*value*/, 
-			Windows::UI::Xaml::Interop::TypeName const& /*targetType*/, 
-			Windows::Foundation::IInspectable const& /*parameter*/, 
+			Windows::Foundation::IInspectable const& /*value*/,
+			Windows::UI::Xaml::Interop::TypeName const& /*targetType*/,
+			Windows::Foundation::IInspectable const& /*parameter*/,
 			hstring const& /*language*/)
 		{
 			throw hresult_not_implemented();
--- FacebookSDK/ScaleConverter.h
+++ FacebookSDK/ScaleConverter.h
@@ -9,15 +9,15 @@ namespace winrt::FacebookSDK::implementation
 		ScaleConverter() { }
 
 		Windows::Foundation::IInspectable Convert(
-			Windows::Foundation::IInspectable const& value, 
-			Windows::UI::Xaml::Interop::TypeName const& targetType, 
-			Windows::Foundation::IInspectable const& parameter, 
+			Windows::Foundation::IInspectable const& value,
+			Windows::UI::Xaml::Interop::TypeName const& targetType,
+			Windows::Foundation::IInspectable const& parameter,
 			hstring const& language);
-		
+
 		Windows::Foundation::IInspectable ConvertBack(
-			Windows::Foundation::IInspectable const& /*value*/, 
-			Windows::UI::Xaml::Interop::TypeName const& /*targetType*/, 
-			Windows::Foundation::IInspectable const& /*parameter*/, 
+			Windows::Foundation::IInspectable const& /*value*/,
+			Windows::UI::Xaml::Interop::TypeName const& /*targetType*/,
+			Windows::Foundation::IInspectable const& /*parameter*/,
 			hstring const& /*language*/)
 		{
 			throw hresult_not_implemented();
